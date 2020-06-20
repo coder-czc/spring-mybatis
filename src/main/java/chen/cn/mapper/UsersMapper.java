b3d0e5c743258d8927c6c4830201751f4f7ae1a4
czc-dreamer <59273679+czc-dreamer@users.noreply.github.com>
2020-06-20 11:15:10 +0800

    Update UsersMapper.java

--- src/main/java/chen/cn/mapper/UsersMapper.java
+++ src/main/java/chen/cn/mapper/UsersMapper.java
@@ -1,19 +1,22 @@
 package chen.cn.mapper;
 
 import chen.cn.entity.Users;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 
 public interface UsersMapper {
 
-    int deleteByPrimaryKey(Integer id);
+    int deleteByPrimaryKey(@Param(value = "id") Integer id);
 
     int insert(Users record);
 
     int insertSelective(Users users);
 
     Users selectByPrimaryKey(Integer id);
+
+    int selectByNameAndPass(@Param(value = "name") String name,@Param(value = "pass") String pass);
 
     List<Users> selectAll();
 
